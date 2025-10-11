269526d8425be00a9882ab729da9aedb45ccb38e
青山重輝 <>
2025-10-11 16:04:56 +0900

    homeへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get '/' => 'home#index'
+  root to: 'home#index'
   get '/about' => 'home#about'
 end
