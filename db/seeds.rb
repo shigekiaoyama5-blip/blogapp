21e3015b9ffbad7f4130dcc814410abe93cdf9dc
青山重輝 <>
2025-10-13 15:35:49 +0900

    記事一覧を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# ダミーデータを作るためのファイル
+
+Article.create({title: '新しい記事だよ', content: '素晴らしい記事です。'})
+Article.create({title: '素晴らしい記事だよ', content: '興味深いです。'})
