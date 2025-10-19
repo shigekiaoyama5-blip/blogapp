e06e54f8b3b22bed514e89c389f8799bc5ced62a
青山重輝 <>
2025-10-19 14:21:58 +0900

    user_id を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_18_082324) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_19_051523) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
