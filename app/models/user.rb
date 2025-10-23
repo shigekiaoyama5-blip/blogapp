eb9c7dc5e735d8b7499a43a7e3ed1e4254341570
青山重輝 <>
2025-10-23 18:16:10 +0900

    プロフィール情報を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,22 @@ class User < ApplicationRecord
   end
 
   def display_name
-    email
+    # if profile && profile.nickname
+      # profile.nickname 
+    # else 
+      # self.email.split('@').first
+    # end
+
+    #　ぼっち演算子
+    profile&.nickname || self.email.split('@').first
+  end
+
+  def birthday
+    profile&.birthday
+  end
+
+  def gender
+    profile&.gender
   end
 
   def prepare_profile
